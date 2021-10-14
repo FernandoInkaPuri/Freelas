e7506ffc17c51a63c560d6afbdbaf76bb557f203
Luiz Fernando <luizfegomez@gmail.com>
2021-10-13 22:16:53 -0300

    Algumas validações e I18N

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
     belongs_to :user
+    validates :title, :description, :skills, :max_value, 
+              :limit_date, :start_date, :end_date, presence: true
 end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,3 +1,4 @@
+I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
 # Permitted locales available for the application
 I18n.available_locales = [:'pt-BR']
 
